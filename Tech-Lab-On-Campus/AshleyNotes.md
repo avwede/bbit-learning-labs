4ec61af15c77f69fc4c5c6523f7eb9d65526a369
avwede <avwede@gmail.com>
2024-09-13 11:53:37 -0400

    Consumer - setupRMQConnection

---
+++ Tech-Lab-On-Campus/AshleyNotes.md
@@ -0,0 +1,14 @@
+# Essential Concepts
+
+## Docker
+`Definition`
+- Integrates software dependencies
+- Quickly spin up software builds
+- Portable and lightweight containers
+- Consistent environment across all platforms
+- Like a virtual machine, but lighter and more efficient
+
+## RabbitMQ
+`Definition`
+- Messaging broker
+- Allows applications to communicate with each other by sending / recieving messages via queues
---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -0,0 +1,65 @@
+import os
+import pika
+from consumer_interface import mqConsumerInterface
+
+# mqConsumer inherits from mqConsumerInterface
+class mqConsumer(mqConsumerInterface):
+    # Constructor - called when the class is first created
+    # -> None indicates the return type
+    def __init__(self, binding_key: str, exchange_name: str, queue_name: str) -> None:
+        # Take in parameters and save it to self
+        # "Self" references the instance of the class itself
+        # Each parameter is defined in mqConsumerInterface
+        self.m_binding_key = binding_key 
+        self.m_queue_name = queue_name
+        self.m_exchange_name = exchange_name
+
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        # pika is a python client for RabbitMQ.
+
+        # Get the RabbitMQ connection URL from the environment var "AMPQ_URL"
+        # Parse the url to extract the params necessary to connect to RabbitMQ
+        connection_params = pika.URLParameters(os.environ["AMPQ_URL"])
+
+        # Connect to RabbitMQ with the extracted params
+        # "Blocking" means that operations on this connection will block/wait until they are completed
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html
+        self.m_connection = pika.BlockingConnection(parameters=connection_params)
+
+        # A channel object is used to communicate with RabbitMQ via the AMQP RPC methods
+        # https://pika.readthedocs.io/en/stable/modules/channel.html
+        self.m_channel = self.m_connection.channel()
+
+        # Ensure that the specified queue exists - if it doesn’t, create it.
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html?highlight=queue_declare#pika.adapters.blocking_connection.BlockingChannel.queue_declare
+        self.m_channel.queue_declare(queue=self.m_queue_name)
+
+        # Ensure that the specified exchange exists - if it doesn’t, create it
+        # "Exchange" is a routing mechanism that receives messages from producers + determines which queues they should be delivered to
+        # Exchanges don't store messages - they route messages based on certain rules or bindings
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html?highlight=exchange_declare#pika.adapters.blocking_connection.BlockingChannel.exchange_declare
+        self.m_channel.exchange_declare(self.m_exchange_name)
+
+        # Bind the queue and exchange
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html?highlight=queue_bind#pika.adapters.blocking_connection.BlockingChannel.queue_bind
+        self.m_channel.queue_bind(
+            queue=self.m_queue_name,
+            routing_key=self.m_binding_key,
+            exchange=self.m_exchange_name,
+        )
+
+        # Set up consumer via basic_consume
+        # Set up callback function for receiving messages via on_message_callback (when the message is received, this callback is invoked)
+        # auto_ack=False - messages aren't automatically acknowledged by RabbitMQ once delivered to the consumer
+        self.m_channel.basic_consume(
+            self.m_queue_name, self.on_message_callback, auto_ack=False
+        )
+        
+
+    def onMessageCallback(self) -> None:
+        pass
+
+    def startConsuming(self) -> None:
+        pass
