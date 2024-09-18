85ab362ca6aaedf97b48d21d2716dd7f5a861731
avwede <avwede@gmail.com>
2024-09-18 14:42:14 -0400

    RMQ connection for producer

--- Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -7,7 +7,39 @@ class mqProducer(mqProducerInterface):
         # Save parameters to class variables
         self.m_routing_key = routing_key
         self.m_exchange_name = exchange_name
-        
+
         # Call setupRMQConnection
         self.setupRMQConnection()
+    
+
+    def setupRMQConnection(self) -> None:
+        # pika is a python client for RabbitMQ.
+
+        # Parse the url to extract the params necessary to connect to RabbitMQ
+        connection_params = pika.URLParameters(os.environ["AMQP_URL"]) 
+
+        # Connect to RabbitMQ with the extracted params
+        # "Blocking" means that operations on this connection will block/wait until they are completed
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html
+        self.m_connection = pika.BlockingConnection(parameters=connection_params)
+
+        # A channel object is used to communicate with RabbitMQ via the AMQP RPC methods
+        # https://pika.readthedocs.io/en/stable/modules/channel.html
+        self.m_channel = self.m_connection.channel()
+
+        # Ensure that the specified queue exists - if it doesn’t, create it.
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html?highlight=queue_declare#pika.adapters.blocking_connection.BlockingChannel.queue_declare
+        self.m_channel.queue_declare(queue=self.m_queue_name)
+
+        # Ensure that the specified exchange exists - if it doesn’t, create it
+        # "Exchange" is a routing mechanism that receives messages from producers + determines which queues they should be delivered to
+        # Exchanges don't store messages - they route messages based on certain rules or bindings
+        # https://pika.readthedocs.io/en/stable/modules/adapters/blocking.html?highlight=exchange_declare#pika.adapters.blocking_connection.BlockingChannel.exchange_declare
+        self.m_channel.exchange_declare(self.m_exchange_name)
+    
+    def publishOrder() -> None:
+        pass
+
+    def __del__() -> None:
+        pass
 
