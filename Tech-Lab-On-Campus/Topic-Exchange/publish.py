d8e5c2cbce3d66a7d2fe5f4392aa754dbb363609
avwede <avwede@gmail.com>
2024-09-18 23:16:30 -0400

    Producer service implementation

--- Tech-Lab-On-Campus/Topic-Exchange/publish.py
+++ Tech-Lab-On-Campus/Topic-Exchange/publish.py
@@ -20,29 +20,46 @@ from solution.producer_sol import mqProducer  # pylint: disable=import-error
 # Producer Service File
 
 def main(ticker: str, price: float, sector: str) -> None:
-    
     # Implement Logic to Create Routing Key from the ticker and sector variable -  Step 2
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    routingKey = f"Stock.{ticker}.{sector}"
 
+    # Remove whitespace
+    routingKey.strip()
 
-    producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
-
+    # Create an intance of the mqProducer class
+    producer = mqProducer(routing_key = routingKey, exchange_name = "Tech Lab Topic Exchange")
 
     # Implement Logic To Create a message variable from the variable EG. "TSLA price is now $500" - Step 3
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    message = f"{ticker} is now ${price}"
     
-    
+    # Publish Message
     producer.publishOrder(message)
 
 if __name__ == "__main__":
 
     # Implement Logic to read the ticker, price and sector string from the command line and save them - Step 1
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    # Create an instance of ArgumentParser 
+    # The description is what will be displayed when you use the --help flag
+    # argparse - https://docs.python.org/3/library/argparse.html
+    parser = argparse.ArgumentParser(
+        description="Process Stock Ticker, Price And Type."
+    )
 
-    sys.exit(main(ticker,price,sector))
+    # Add command line arguments
+    # Expected values of arguments are all strings
+    # All arguments are mandatory
+    parser.add_argument(
+        "-t", "--ticker", type=str, help="Stock Ticker", required=True
+    )
+    parser.add_argument(
+        "-p", "--price", type=str, help="Stock Price", required=True
+    )
+    parser.add_argument(
+        "-s", "--sector", type=str, help="Stock Sector", required=True
+    )
+
+    # Parse arguments and assign them to `args`
+    # The arguments are now accessible on the args object via args.sector or args.queue
+    args = parser.parse_args()
+
+    sys.exit(main(args.ticker, args.price, args.sector))
